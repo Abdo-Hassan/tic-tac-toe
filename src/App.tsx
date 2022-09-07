3cd8a447af0d055f42c321f62e93a2611cb2c8ab
abdo <abdoholmes97@gmail.com>
2022-09-07 23:27:32 +0200

    style: install lobster font

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
 function App() {
   return (
-    <div className='App'>
-      <p className='read-the-docs'>test</p>
+    <div className='h-full p-8 text-slate-800 bg-gradient-to-r from-cyan-500 to-blue-500'>
+      <h1 className='text-center text-5xl md-4 font-display text-white'>
+        Tic Tac Toe Game
+      </h1>
+
+      <div>
+        <div>Board Goes Here</div>
+        <div>Score Goes Here</div>
+      </div>
     </div>
   );
 }
